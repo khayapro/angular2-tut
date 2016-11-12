26f7e6c118739079d40a2dc8661ab6ec9fb75c20
Khaya Mathebula <kmathebula@fnb.co.za>
2016-11-12 17:37:31 +0200

    editing the hero's details, using one-way & two-way databinding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,33 @@
 import {Component} from '@angular/core';
 
+export class Hero{
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Hello Angular some</h1>
+        <h1>{{title}}</h1> 
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="hero name">    
+        </div>
     `
 })
 
-export class AppComponent {}
+export class AppComponent {
+
+    title: string;
+    hero: Hero;
+
+    constructor(){
+        this.title = 'Tour of Heroes';
+        this.hero = {
+            id: 1,
+            name: 'Windostorm'
+        }
+    }
+}
